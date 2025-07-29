442a0f81f2ab501c779a123c2b23576a736825a9
nightlighttw <david97099@gmail.com>
2025-07-29 10:15:57 +0800

    Update tcm_sd task configurations and evaluation script

--- eval.sh
+++ eval.sh
@@ -1,11 +1,11 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 accelerate launch --num_processes 1 --main_process_port 30000 -m lmms_eval \
     --model gpt \
-    --model_args model_version=gpt-4o-mini,api_url=http://172.22.0.3:9621/v1/chat/completions\
-    --tasks paper_rqa_o1_60000 \
+    --model_args model_version=gpt-4o-mini \
+    --tasks tcm_sd_rc_five_upperbound \
     --batch_size auto \
     --log_samples \
     --output_path ./logs/ \
-    --wandb_args project=test_log,name=lightrag_mix-1 \
-    --limit 5
-
+    --wandb_args project=tcm_sd_rc_five,name=upperbound \
+    # --limit 5
